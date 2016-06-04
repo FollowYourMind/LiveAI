45a0b31f836c8726dd6909fc2861c695a61c2459
masaMikam <masaMikam@outlook.jp>
2016-06-05 01:01:11 +0900

    ver6.04

---
+++ operate_sql.py
@@ -0,0 +1,347 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# from setup import core_sql, talk_sql, twlog_sql, wordnet_sql
+from sql_models import *
+import dialog_generator
+import _
+from _ import p, d, MyObject, MyException
+def save_stats(stats_dict = {'whose': 'sys', 'status': '', 'number': 114514}):
+	try:
+		core_sql.create_tables([Stats], True)
+		with core_sql.transaction():
+			t = Stats(**stats_dict)
+			t.save()
+			core_sql.commit()
+	except Exception as e:
+		print(e)
+		core_sql.rollback()
+
+def get_stats(whose = 'sys', status = '', n = 100):
+	try:
+		with core_sql.transaction():
+			stats_data = Stats.select().where(Stats.whose ==  whose, Stats.status == status).order_by(Stats.time.desc()).limit(n)
+			data_ls = [(data.number, data.time) for data in stats_data]
+			return data_ls
+	except Exception as e:
+		print(e)
+		core_sql.rollback()
+def get_core_info(whose_info = 'LiveAI_Umi', info_label = 'test', standard_dic = {'Char1': '', 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = False):
+	core_sql.create_tables([CoreInfo], True)
+	try:
+		with core_sql.transaction():
+			core_info, is_created = CoreInfo.get_or_create(whose_info = whose_info, info_label = info_label)
+			if is_created or is_update:
+				standard_dic['whose_info'] = whose_info
+				standard_dic['info_label'] = info_label
+				ci = CoreInfo(**standard_dic)
+				ci.save()
+				return ci
+			return core_info
+	except Exception as e:
+		print(e)
+		core_sql.rollback()
+def save_task(taskdict = {'who':'_umiS', 'what': 'call', 'to_whom': '_apkX', 'when':datetime.utcnow()+timedelta(hours = 9)}):
+	try:
+		core_sql.create_tables([Task], True)
+		with core_sql.transaction():
+			t = Task(**taskdict)
+			t.save()
+			core_sql.commit()
+			p(''.join(['TASK_SAVED]',taskdict['who'], '_', taskdict['what']]))
+	except Exception as e:
+		print(e)
+		core_sql.rollback()
+
+def search_tasks(when = datetime.now(), who = '_umiS', n = 10):
+	try:
+		with core_sql.transaction():
+			active = Task.select().where(~Task.status == 'end')
+			if who == '':
+				tasks = active.where(Task.when < when).order_by(Task.id.desc()).limit(n)
+			else:
+				tasks = active.where(Task.when < when, Task.who == who).order_by(Task.id.desc()).limit(n)
+			tasklist = [task.__dict__['_data'] for task in tasks]
+			return tasklist
+	except Exception as e:
+		print(e)
+		core_sql.rollback()
+
+def update_task(taskid = None, who_ls = [], kinds = [], taskdict = {'who':'', 'what': 'call', 'to_whom': '_apkX', 'when':datetime.utcnow()}):
+	try:
+		with core_sql.transaction():
+			if who_ls:
+				if not kinds:
+					task = Task.update(**taskdict).where(Task.id == taskid)
+				elif not taskid:
+					task = Task.update(**taskdict).where(Task.what << kinds, Task.who << who_ls)
+				else:
+					task = Task.update(**taskdict).where(Task.id == taskid, Task.what << kinds, Task.who << who_ls)
+			else:
+				if not kinds:
+					task = Task.update(**taskdict).where(Task.id == taskid)
+				elif not taskid:
+					task = Task.update(**taskdict).where(Task.what << kinds)
+				else:
+					task = Task.update(**taskdict).where(Task.id == taskid, Task.what << kinds)
+			task.execute()
+	except Exception as e:
+		print(e)
+		core_sql.rollback()
+
+#####twlog_sql#######
+def save_tweet_status(status, tmp):
+	try:
+		twlog_sql.create_tables([Tweets], True)
+		with twlog_sql.transaction():
+			Tweets.create(
+				status_id = int(status['id_str']),
+				screen_name = status['user']['screen_name'],
+				name = status['user']['name'],
+				text = status['text'],
+				user_id = status['user']['id_str'],
+				in_reply_to_status_id_str = status['in_reply_to_status_id_str'],
+				bot_id = tmp['bot_id'],
+				createdAt = datetime.utcnow(),
+				updatedAt = datetime.utcnow()
+			)
+			twlog_sql.commit()
+			# print('SAVED]')
+	except Exception as e:
+		print(e)
+		twlog_sql.rollback()
+
+def get_tweet_dialog(status_id = 1):
+	try:
+		# twlog_sql.create_tables([Tweets], True)# 第二引数がTrueの場合、存在している場合は、作成しない
+		with twlog_sql.transaction():
+			return Tweets.select().where(Tweets.status_id == status_id).get().__dict__['_data']
+	except Exception as e:
+		print(e)
+		twlog_sql.rollback()
+		return ''
+
+def save_tweet_dialog(status, tmp, logtext = ''):
+	try:
+		# twlog_sql.create_tables([TwDialog], True)# 第二引数がTrueの場合、存在している場合は、作成しない
+		with twlog_sql.transaction():
+			if not logtext:
+				twlog = Tweets.select().where(Tweets.status_id == int(status['in_reply_to_status_id_str'])).get().__dict__['_data']
+				screen_name = twlog['screen_name']
+				logtext = _.clean_text(twlog['text'])
+				SID = '/'.join([str(twlog['status_id']), status['id_str']])
+			else:
+				screen_name = tmp['bot_id']
+				SID = ''.join([status['id_str'], 'DM'])
+			kws = [key[0] for key in TFIDF.calcKWs(logtext, length = 1, needs = {'固有名詞', '名詞'})]
+			TwDialog.create(
+				SID = SID,
+				KWs = '</>'.join(kws),
+				nameA = screen_name,
+				textA = logtext,
+				nameB = status['user']['screen_name'],
+				textB = _.clean_text(status['text']),
+				posi = 1,
+				nega = 0,
+				bot_id = tmp['bot_id'],
+				createdAt = datetime.utcnow(),
+				updatedAt = datetime.utcnow()
+			)
+			twlog_sql.commit()
+			return True
+	except Exception as e:
+		print(e)
+		twlog_sql.rollback()
+		return False
+
+def get_twlog_list(n = 1000, UserList = ['sousaku_umi', 'umi0315_pokemon'], BlackList = ['hsw37', 'ry72321', 'MANI_CHO_8', 'HONO_HONOKA_1', 'MOEKYARA_SAIKOU', 'megmilk_0308'], contains = ''):
+	try:
+		# twlog_sql.create_tables([Tweets], True)# 第二引数がTrueの場合、存在している場合は、作成しない
+		with twlog_sql.transaction():
+			if not UserList:
+				tweets = Tweets.select().where(Tweets.text.contains(contains), ~Tweets.text.contains('RT'), ~Tweets.screen_name << BlackList, ~Tweets.text.contains('【')).order_by(Tweets.createdAt.desc()).limit(n)
+			else:
+			 	tweets = Tweets.select().where(Tweets.screen_name << UserList , ~Tweets.screen_name << BlackList, ~Tweets.text.contains('RT'), ~Tweets.text.contains('【')).order_by(Tweets.createdAt.desc()).limit(n)
+		tweetslist = [_.clean_text(tweet.text) for tweet in tweets]
+		return tweetslist
+	except Exception as e:
+		print(e)
+		twlog_sql.rollback()
+
+def getPhrase(s_type = '', status = '', n = 10, character = 'sys'):
+	p('old get_phrase')
+	return get_phrase(s_type = s_type, status = status, n = n, character = character)
+def get_phrase(s_type = '', status = '', n = 10, character = 'sys'):
+	try:
+		with core_sql.transaction():
+			if character == 'sys':
+				Ps = Phrases.select().where(Phrases.status == status).limit(n)
+			elif not status:
+				Ps = Phrases.select().where(Phrases.s_type == s_type, Phrases.character == character).limit(n)
+			elif not s_type:
+				Ps = Phrases.select().where(Phrases.status == status, Phrases.character == character).limit(n)
+			else:
+				Ps = Phrases.select().where(Phrases.s_type == s_type, Phrases.status == status, Phrases.character == character).limit(n)
+			if len(Ps) == 0:
+				Ps = Phrases.select().where(Phrases.status == status).limit(n)
+				return ''.join([np.random.choice([p.phrase for p in Ps]), '\n(代:[', status, ']of\'', character, '\')'])
+			else:
+				return np.random.choice([p.phrase for p in Ps])
+
+	except Exception as e:
+		print(e)
+		core_sql.rollback()
+		return ''.join(['...:[', status, '] by \'', character, '\''])
+def savePhrase(phrase, author = '_mmkm', status = 'mid', s_type = 'UserLearn', character = 'sys'):
+	p('old save_phrase')
+	return save_phrase(s_type = s_type, author = author, phrase = phrase, status = status, n = n, character = character)
+def save_phrase(phrase, author = '_mmkm', status = 'mid', s_type = 'UserLearn', character = 'sys'):
+	try:
+		core_sql.create_tables([Phrases], True)# 第二引数がTrueの場合、存在している場合は、作成しない
+		with core_sql.transaction():
+			P, is_created = Phrases.get_or_create(phrase = phrase)
+			if is_created:
+				P.phrase = phrase
+				P.framework = phrase
+				P.s_type = s_type
+				P.status = status
+				P.ok_cnt = 1
+				P.ng_cnt = 0
+				P.author = author
+				# P.createdAt
+				P.character = character
+				P.save()
+				core_sql.commit()
+				return True
+			return True
+	except Exception as e:
+		print(e)
+		core_sql.rollback()
+		return False
+
+def eval_phrase(phrase, ok_add = 0, ng_add = 1):
+	try:
+		# core_sql.create_tables([Tweets], True)# 第二引数がTrueの場合、存在している場合は、作成しない
+		with core_sql.transaction():
+			P = Phrases.select().where(Phrases.phrase == phrase).get()
+			P.ok_cnt = P.ok_cnt + ok_add
+			P.ng_cnt = P.ng_cnt + ng_add
+			P.save()
+			core_sql.commit()
+			return True
+	except Exception as e:
+		print(e)
+		core_sql.rollback()
+		return False
+def get_userinfo(screen_name):
+	return read_userinfo(screen_name = screen_name)
+
+def read_userinfo(screen_name):
+	try:
+		# core_sql.create_tables([Users], True)# 第二引数がTrueの場合、存在している場合は、作成しない
+		with core_sql.transaction():
+			try:
+				userinfo, is_created = Users.get_or_create(screen_name = screen_name)
+			except Exception as e:
+				if screen_name == 'h_y_ok':
+					screen_name = '例外h_y_ok'
+					userinfo, is_created = Users.get_or_create(screen_name = screen_name)
+				else:
+					print(e)
+					is_created = False
+			if is_created:
+				userinfo.name = screen_name
+				userinfo.nickname = screen_name
+				userinfo.cnt = 0
+				userinfo.total_cnt = 0
+				userinfo.reply_cnt = 0
+				userinfo.exp = 0
+				userinfo.mode = 'dialog'
+				userinfo.context = ''
+				userinfo.time = datetime.utcnow()
+			try:
+				userinfodata = userinfo._data
+			except:
+				userinfodata = ''
+			return userinfodata, is_created
+	except Exception as e:
+		print(e)
+		core_sql.rollback()
+
+def save_userinfo(userstatus):
+	try:
+		# core_sql.create_tables([Tweets], True)# 第二引数がTrueの場合、存在している場合は、作成しない
+		with core_sql.transaction():
+			try:
+				userinfo = Users(**userstatus)
+				userinfo.save()
+			except Exception as e:
+				print(e)
+				return False, userstatus
+			core_sql.commit()
+			return True, userstatus
+	except Exception as e:
+	  core_sql.rollback()
+	  return False, userstatus
+def get_wordnet_result(lemma):
+	n = 10
+	langs_ls = ['jpn', 'eng']
+	langs_ls = ['jpn']
+	wn_relation = {}
+	def convert_synset_into_words(target_synset):
+		try:
+			same_sense_set = Sense.select().where(Sense.synset == target_synset).limit(n)
+			same_sense_wordid_ls = [same_sense_word.wordid for same_sense_word in same_sense_set]
+			same_sense_words = Word.select().where(Word.wordid << same_sense_wordid_ls, Word.lang << langs_ls).limit(n)
+			same_sense_lemma_ls = [same_sense_word.lemma for same_sense_word in same_sense_words]
+			return same_sense_lemma_ls
+		except:
+			return []
+	try:
+		with wordnet_sql.transaction():
+			W = Word.select().where(Word.lemma == lemma).get()
+			selected_wordid = W.wordid
+			wn_sense = Sense.select().where(Sense.wordid == selected_wordid).get()
+			selected_synset = wn_sense.synset
+			coordinated_lemma_ls = convert_synset_into_words(target_synset = selected_synset)
+			synlinks = Synlink.select().where(Synlink.synset1 == selected_synset).limit(n)
+			wn_relation = {link: words_ls for link, words_ls in [(synlink.link, convert_synset_into_words(target_synset = synlink.synset2))  for synlink in synlinks] if words_ls}
+			wn_relation['coordinate'] = coordinated_lemma_ls
+			return wn_relation
+	except Exception as e:
+		print(e)
+		wordnet_sql.rollback()
+class BotProfile(MyObject):
+	def __init__(self, bot_id = 'a'):
+		self.bot_id = bot_id
+		self.read()
+	def save(self):
+		get_core_info(whose_info = self.bot_id, info_label = 'name', standard_dic = {'Char1': self.name, 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = True)
+		get_core_info(whose_info = self.bot_id, info_label = 'description', standard_dic = {'Char1': self.description, 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = True)
+		get_core_info(whose_info = self.bot_id, info_label = 'location', standard_dic = {'Char1': self.location, 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = True)
+		get_core_info(whose_info = self.bot_id, info_label = 'url', standard_dic = {'Char1': self.url, 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = True)
+		get_core_info(whose_info = self.bot_id, info_label = 'abs_icon_filename', standard_dic = {'Char1': self.abs_icon_filename, 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = True)
+		get_core_info(whose_info = self.bot_id, info_label = 'abs_banner_filename', standard_dic = {'Char1': self.abs_banner_filename, 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = True)
+	def read(self):
+		self.name = get_core_info(whose_info = self.bot_id, info_label = 'name', standard_dic = {'Char1': '', 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = False)._data['Char1']
+		self.description = get_core_info(whose_info = self.bot_id, info_label = 'description', standard_dic = {'Char1': '', 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = False)._data['Char1']
+		self.location = get_core_info(whose_info = self.bot_id, info_label = 'location', standard_dic = {'Char1': '', 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = True)._data['Char1']
+		self.url = get_core_info(whose_info = self.bot_id, info_label = 'url', standard_dic = {'Char1': '', 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = False)._data['Char1']
+		self.abs_icon_filename = get_core_info(whose_info = self.bot_id, info_label = 'abs_icon_filename', standard_dic = {'Char1': '', 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = False)._data['Char1']
+		self.abs_banner_filename = get_core_info(whose_info = self.bot_id, info_label = 'abs_banner_filename', standard_dic = {'Char1': '', 'Char2': '', 'Char3': '', 'Int1':0, 'Int2':0}, is_update = False)._data['Char1']
+if __name__ == '__main__':
+	# umi = read_userinfo('h_y_ok')
+	# umi = get_twlog_list(n = 10)
+	# umi = get_phrase(status = 'ぬるぽ', n = 1)
+	# p(umi)
+	# a= get_wordnet_result('米')
+	bp = BotProfile('a')
+	# bp.name = 'てすとおととおお'
+	# bp.save()
+	p(bp)
+	# a =read_userinfo(screen_name = 'masaMikam')[0]._data
+	# p(a)
+	status = {'favorite_count': 0, 'created_at': 'Wed Feb 17 14:54:01 +0000 2016', 'contributors': None, 'truncated': False, 'in_reply_to_user_id_str': None, 'retweet_count': 0, 'id': 699970059683414016, 'in_reply_to_status_id_str': None, 'geo': None, 'entities': {'hashtags': [], 'urls': [], 'symbols': [], 'user_mentions': []}, 'id_str': '691170059683414016', 'in_reply_to_screen_name': None, 'is_quote_status': False, 'timestamp_ms': '1455720841700', 'coordinates': None, 'in_reply_to_status_id': None, 'filter_level': 'low', 'retweeted': False, 'in_reply_to_user_id': None, 'source': '<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>', 'favorited': False, 'user': {'protected': False, 'created_at': 'Sun Mar 31 01:35:13 +0000 2013', 'utc_offset': 32400, 'favourites_count': 27, 'follow_request_sent': None, 'following': None, 'profile_image_url': 'http://pbs.twimg.com/profile_images/681463777951236096/SbnleYeJ_normal.jpg', 'profile_background_tile': False, 'description': '名前:つゆり きさめ/学生ラブライバー/絶叫勢/ぼっち勢/海未推し/善子推し(仮)/このすば/めぐみんはいいぞ/内田彩/詳細はツイプロ/+aで最近FFの比例がおかしい事に気がついたんでスパムを除く人に見つけ次第フォロー返してます。', 'profile_text_color': '333333', 'friends_count': 1481, 'time_zone': 'Tokyo', 'profile_sidebar_border_color': 'BDDCAD', 'profile_image_url_https': 'https://pbs.twimg.com/profile_images/681463777951236096/SbnleYeJ_normal.jpg', 'screen_name': 'tuyuri_kisame', 'default_profile_image': False, 'statuses_count': 40262, 'name': '栗花落 樹雨', 'is_translator': False, 'profile_background_image_url_https': 'https://abs.twimg.com/images/themes/theme16/bg.gif', 'followers_count': 1921, 'location': '神奈川県東部', 'geo_enabled': False, 'verified': False, 'notifications': None, 'profile_banner_url': 'https://pbs.twimg.com/profile_banners/1317490188/1455626298', 'listed_count': 33, 'profile_background_color': '9AE4E8', 'profile_sidebar_fill_color': 'DDFFCC', 'profile_link_color': '0084B4', 'default_profile': False, 'url': 'http://twpf.jp/tuyuri_kisame', 'profile_use_background_image': True, 'contributors_enabled': False, 'id': 1317490188, 'lang': 'ja', 'id_str': '1317490188', 'profile_background_image_url': 'http://abs.twimg.com/images/themes/theme16/bg.gif'}, 'place': None, 'text': 'トサカのないことりちゃん…( ˘ω˘ )', 'lang': 'ja'}
+	# save_tweet_status(status)
+	s = '酒'
+
+
---
+++ twtr_functions.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#MY MODULEs
+#variables
+from setup import *
+import _
+from _ import p, d, MyObject, MyException
+import natural_language_processing
+import operate_sql
+import main
+class StreamListener(tweepy.streaming.StreamListener):
+	def __init__(self, bot_id):
+		super().__init__()
+		p(bot_id)
+		self.bot_id = bot_id
+		self.response_main = main.StreamResponseFunctions(bot_id)
+		self.response_main.on_initial_main()
+	def __del__(self):
+		p(self.bot_id, 'stopping streaming...')
+	def on_friends(self, friends):
+		return self.response_main.on_friends_main(friends)
+	def on_limit(self, track):
+		p(track)
+		raise MyException
+	def on_status(self, status):
+		return self.response_main.on_status_main(status._json)
+	def on_direct_message(self,status):
+		return self.response_main.on_direct_message_main(status._json)
+	def on_event(self, status):
+		return self.response_main.on_event_main(status._json)
+	def on_error(self,status):
+		p('cannot get')
+		raise MyException
+	def on_timeout(self):
+		raise MyException
+def get_twtr_auth(auth_dic):
+	try:
+		CONSUMER_KEY = auth_dic['consumer_key']
+		CONSUMER_SECRET = auth_dic['consumer_secret']
+		auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+		ACCESS_TOKEN = auth_dic['access_token_key']
+		ACCESS_SECRET = auth_dic['access_token_secret']
+		auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+		return auth
+	except:
+		pass
+class TwtrTools:
+	def __init__(self, bot_id = 'LiveAIs'):
+		self.bot_id = bot_id
+		api_keys = cfg['twtr']
+		twtr_auths = {key: get_twtr_auth(value) for key, value in api_keys.items()}
+		twtr_apis = {key: tweepy.API(value, wait_on_rate_limit = True) for key, value in twtr_auths.items()}
+		self.twtr_auth = twtr_auths[bot_id]
+		self.twtr_api = twtr_apis[bot_id]
+	def Stream(self):
+		auth = self.twtr_auth
+		stream = tweepy.Stream(auth = auth, listener = StreamListener(self.bot_id), async = True, secure=True)
+		while True:
+			try:
+				stream.userstream()
+			except Exception as e:
+				d(e)
+				time.sleep(100)
+				stream = tweepy.Stream(auth = auth, listener = StreamListener(self.bot_id), async = True, secure=True)
+	def send(self, ans, screen_name = '', status_id = '', imgfile = '', mode = 'dm', try_cnt = 0):
+		if mode == 'dm':
+			return self.send_direct_message(ans = ans, screen_name = screen_name)
+		elif mode == 'open':
+			return self.tweet(ans = ans, screen_name = '', status_id = '', imgfile = imgfile, is_debug = is_debug, try_cnt = try_cnt)
+		else:
+			return self.tweet(ans = ans, screen_name = screen_name, status_id = status_id, imgfile = imgfile, is_debug = is_debug, try_cnt = try_cnt)
+
+	def tweet(self, ans, screen_name = '', status_id = '', imgfile = '', is_debug = False,  try_cnt = 0):
+		p('old')
+		return self.send_tweet(self, ans, screen_name = '', status_id = '', imgfile = '', is_debug = False, try_cnt = 0)
+	def send_tweet(self, ans, screen_name = '', status_id = '', imgfile = '', is_debug = False,  try_cnt = 0):
+		try:
+			if screen_name:
+				ans1 = ''.join(['@', screen_name,' ', ans]).replace('@@', '@')
+			else:
+				ans1 = ans
+
+			if len(ans) > 140:
+				is_split = True
+				ans2 = ''.join([ans1[0:139], '…'])
+			else:
+				is_split = False
+				ans2 = ans1
+			if imgfile:
+				if not is_debug:
+					tweetStatus = self.twtr_api.update_with_media(imgfile, status = ans2, in_reply_to_status_id = status_id)
+					print('[Tweet.IMG.OK] @', screen_name, ' ', ans2)
+				else:
+					print('[Debug][Tweet.IMG.OK] @', screen_name, ' ', ans2)
+			else:
+				if not is_debug:
+					tweetStatus = self.twtr_api.update_status(status = ans2, in_reply_to_status_id = status_id)
+					print('[Tweet.OK] @', screen_name, ' ', ans2)
+				else:
+					print('[Debug][Tweet.OK] @', screen_name, ' ', ans2)
+			# 140字越えの際は、分割ツイート
+			if is_split:
+				if screen_name:
+					# self.tweet(''.join(['...', ans[139:]]), screen_name = screen_name, status_id = status_id, tmp = tmp)
+					try_cnt += 1
+					return self.tweet(''.join(['...', ans[139:]]), screen_name = screen_name, status_id = status_id, is_debug = is_debug, try_cnt = try_cnt)
+				else:
+					return True
+			else:
+				return True
+		except tweepy.error.TweepError as e:
+			print('[ERR][Tweet.TweepError] @', screen_name, ' ', ans)
+			p(e)
+			if e.response and e.response.status == 403:
+				print('403')
+				# tmp['tweetStatus']['tempStop_since'] = tmp["clocks"]['now']
+				return False
+			else:
+				return True
+		except Exception as e:
+			print('[Tweet.ERR] @', screen_name, ' ', ans)
+			print(e)
+			return False
+
+	def send_direct_message(self, ans, screen_name = '', is_debug = False, try_cnt = 0):
+		try:
+			if not is_debug:
+				tweetStatus = self.twtr_api.send_direct_message(screen_name = screen_name, text = ans)
+				print('[DM.OK] @', screen_name, ' ', ans)
+			else:
+				print('[Debug][DM.OK] @', screen_name, ' ', ans2)
+			return True
+		except tweepy.error.TweepError as e:
+			print('[ERR][DM.TweepError] @', screen_name, ' ', ans)
+			if e.response and e.response.status == 403:
+				print('403')
+				return False
+			else:
+				return True
+		except Exception as e:
+			print('[DM.ERR] @', screen_name, ' ', ans)
+			print(e)
+			return False
+
+	# def send_direct_message(self, ans, screen_name = '', tmp = {'tweetStatus': {'is_debug':False, 'isSplitTweet': False, 'tempStop_since':0}}):
+	# 	try:
+	# 		if not tmp['tweetStatus']['is_debug']:
+	# 			tweetStatus = self.twtr_api.send_direct_message(screen_name = screen_name, text = ans)
+	# 			print('[DM.OK] @', screen_name, ' ', ans)
+	# 		else:
+	# 			print('[Debug][DM.OK] @', screen_name, ' ', ans2)
+	# 		return True, tmp
+	# 	except tweepy.error.TweepError as e:
+	# 		print('[ERR][DM.TweepError] @', screen_name, ' ', ans)
+	# 		if e.response and e.response.status == 403:
+	# 			print('403')
+	# 			tmp['tweetStatus']['tempStop_since'] = tmp['now']
+	# 			return False, tmp
+	# 		else:
+	# 			return True, tmp
+	# 	except Exception as e:
+	# 		print('[DM.ERR] @', screen_name, ' ', ans)
+	# 		print(e)
+	# 		return False, tmp
+
+	def getTrendwords(self, mode = 'withoutTag'):
+		# 'locations': [{'woeid': 23424856, 'name': 'Japan'}]
+		trends = self.twtr_api.trends_place(id = 23424856)[0]['trends']
+		if mode == 'withoutTag':
+			return [trend['name'] for trend in trends if not '#' in trend['name']]
+		elif mode == 'withTag':
+			trendtags = [trend['name'] for trend in trends if '#' in trend['name']]
+			trendwords = [trend['name'] for trend in trends if not '#' in trend['name']]
+			return trendwords, trendtags
+		else:
+			return [trend['name'] for trend in trends]
+	def update_profile(self, name, description, location, url = '', filename = '', BGfilename = '', Bannerfilename = ''):
+		try:
+			self.twtr_api.update_profile(name = name, url = url,  location = location, description = description)
+			if filename:
+				self.twtr_api.update_profile_image(filename)
+			if BGfilename:
+				self.twtr_api.update_profile_background_image(BGfilename)
+			if Bannerfilename:
+				self.twtr_api.update_profile_banner(Bannerfilename)
+			return True
+		except Exception as e:
+			print(e)
+			return False
+	def is_create_list_success(self, name, mode = 'private', description = ''):
+		try:
+			self.twtr_api.create_list(name = name, mode = mode, description = description)
+			return True
+		except:
+			return False
+	def get_listmembers_all(self, username, listname):
+		try:
+			return [UserObject.screen_name for UserObject in tweepy.Cursor(self.twtr_api.list_members, username, listname).items()]
+		except tweepy.error.TweepError as e:
+			if e.api_code == '34':
+				if username == self.bot_id:
+					self.is_create_list_success(name = listname)
+			return []
+		except:
+			return []
+	# def get_lists(self):
+	# 	return [UserObject.__dict__ for UserObject in tweepy.Cursor(self.twtr_api.lists, username, listname).items()]
+	def give_fav(self, status_id):
+		try:
+			self.twtr_api.create_favorite(id = status_id)
+		except :
+			return False
+		else:
+			return True
+	def get_userinfo(self, screen_name):
+		try:
+			return self.twtr_api.get_user(screen_name = screen_name)._json
+		except :
+			pass
+	def is_destroy_friendship_success(self, screen_name):
+		try:
+			self.twtr_api.destroy_friendship(screen_name = screen_name)
+			return True
+		except:
+			return False
+	def is_create_friendship_success(self, screen_name):
+		try:
+			self.twtr_api.create_friendship(screen_name = screen_name)
+			return True
+		except:
+			return False
+	def convert_direct_message_to_tweet_status(self, status):
+  		s= status['direct_message']
+  		s['user'] = {}
+  		s['user']['screen_name'] = s['sender_screen_name']
+  		s['user']['name'] = s['sender']['name']
+  		s['user']['id_str'] = s['sender']['id_str']
+  		s['in_reply_to_status_id_str'] = None
+  		s['in_reply_to_screen_name'] = self.bot_id
+  		s['extended_entities'] = s['entities']
+  		s['retweeted'] = False
+  		s['is_quote_status'] = False
+  		return s
+	def download_userobject_urls(self, userobject, DIR = DIRusers):
+		screen_name = userobject.screen_name
+		USERDIR = '/'.join([DIR, screen_name])
+		if not os.path.exists(USERDIR):
+			os.mkdir(USERDIR)
+		try:
+			userobject.abs_icon_filename = _.saveImg(media_url = userobject.profile_image_url.replace('_normal', ''), DIR = USERDIR, filename = ''.join([screen_name, '_icon.jpg']))
+		except Exception as e:
+			print('[ERR]imitate.icon')
+			print(e)
+			userobject.abs_icon_filename = ''
+		try:
+			userobject.abs_background_filename = _.saveImg(media_url = userobject.profile_background_image_url, DIR = USERDIR, filename = ''.join([screen_name, '_background.jpg']))
+		except Exception as e:
+			print('[ERR]imitate.bg')
+			print(e)
+			userobject.abs_background_filename = ''
+		try:
+			userobject.abs_banner_filename = _.saveImg(media_url = userobject.profile_banner_url, DIR = USERDIR, filename = ''.join([screen_name, '_banner.jpg']))
+		except Exception as e:
+			print('[ERR]imitate.banner')
+			print(e)
+			userobject.abs_banner_filename = ''
+		return userobject
+	def imitate(self, screen_name, DIR = DIRusers):
+		try:
+			user = self.twtr_api.get_user(screen_name = screen_name)
+			user = self.download_userobject_urls(user, DIR = DIR)
+			alt_name = user.name.replace(' ', '')
+			alt_description = user.description
+			is_following = user.following
+			if not is_following:
+				return False
+
+			self.update_profile(name = alt_name, description = alt_description, location = ''.join(['まねっこ中@', screen_name]), url = '', filename = user.abs_icon_filename, BGfilename = user.abs_background_filename, Bannerfilename = user.abs_banner_filename)
+			return True
+		except Exception as e:
+			print('[ERR]imitate')
+			print(e)
+			return False
+
+if __name__ == '__main__':
+	twf = TwtrTools('LiveAI_Rin')
+	# twf.Stream()
+	# ans = twf.get_listmembers_all(username = 'LiveAI_Rin' , listname = 'BOaaa')
+	ans = twf.twtr_api.me()
+	p(ans)
+	print(ans.description)
+
+	# twf.imitate(screen_name = 'LiveAI_Umi', DIR = DIRusers)
+	# twf.send(ans = 'testです', screen_name = '_mmkm', status_id = '', imgfile = '', mode = 'dm',  trycnt = 0)
+
+
+
+
+
+
+
+
+
